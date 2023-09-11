0a8d736282cbcdefec757c368cdc538f0cae0e4c
Christopher Navarro <navarro.christopher16@yahoo.com>
2023-09-10 23:33:39 -0700

    Path to home page and api started

--- src/CharacterPage.js
+++ src/CharacterPage.js
@@ -1,12 +1,23 @@
 import React from "react";
+import axios from "axios";
 
-function CharPage(){
-    return(
-        <div>
-        <h1>Characters</h1>
-        </div>
-    );
+const API_BASE_URL = "https://www.anapioficeandfire.com/api";
+
+async function Characters() {
+  const charList = await axios.get(`${API_BASE_URL}/characters`);
+  console.log(charList["data"]["name"]);
+}
+
+function CharPage() {
+  Characters();
+  return (
+    <div>
+      <h1>Characters</h1>
+      <h2>
+        <a href="/">Home Page</a>
+      </h2>
+    </div>
+  );
 }
 
 export default CharPage;
-
--- src/HousesPage.js
+++ src/HousesPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function HousesPage(){
-    return(
-        <div>
-        <h1>Houses</h1>
-        </div>
-    );
+function HousesPage() {
+  return (
+    <div>
+      <h1>Houses</h1>
+      <h2>
+        <a href="/">Home Page</a>
+      </h2>
+    </div>
+  );
 }
 
 export default HousesPage;
